57ce43251e33caf505bfc72d440bb7880481a6f0
Assaf Giladi <assaf@weka.io>
2023-06-14 20:47:13 +0300

    refactor: remove multi subnet support on azure

--- deploy/deploy.go
+++ deploy/deploy.go
@@ -21,7 +21,6 @@ type DeploymentParams struct {
 	InstallDpdk          bool
 	NicsNum              string
 	Gateways             []string
-	Subnets              []string
 }
 
 type DeployScriptGenerator struct {
@@ -38,7 +37,6 @@ func (d *DeployScriptGenerator) GetDeployScript() string {
 	getCoreIdsFunc := bash_functions.GetCoreIds()
 	getNetStrForDpdkFunc := bash_functions.GetNetStrForDpdk()
 	gateways := strings.Join(d.Params.Gateways, " ")
-	subnets := strings.Join(d.Params.Subnets, " ")
 
 	template := `
 	#!/bin/bash
@@ -52,7 +50,6 @@ func (d *DeployScriptGenerator) GetDeployScript() string {
 	NICS_NUM=%s
 	INSTALL_DPDK=%t
 	GATEWAYS="%s"
-	SUBNETS="%s"
 
 	# clusterize function definition
 	%s
@@ -78,11 +75,11 @@ func (d *DeployScriptGenerator) GetDeployScript() string {
 	get_core_ids $NUM_FRONTEND_CONTAINERS frontend_core_ids
 
 	if [[ $INSTALL_DPDK == true ]]; then
-		getNetStrForDpdk 1 $(($NUM_DRIVE_CONTAINERS+1)) "$GATEWAYS" "$SUBNETS"
+		getNetStrForDpdk 1 $(($NUM_DRIVE_CONTAINERS+1)) "$GATEWAYS"
 		sudo weka local setup container --name drives0 --base-port 14000 --cores $NUM_DRIVE_CONTAINERS --no-frontends --drives-dedicated-cores $NUM_DRIVE_CONTAINERS --failure-domain $FAILURE_DOMAIN --core-ids $drive_core_ids $net --dedicate
-		getNetStrForDpdk $((1+$NUM_DRIVE_CONTAINERS)) $((1+$NUM_DRIVE_CONTAINERS+$NUM_COMPUTE_CONTAINERS )) "$GATEWAYS" "$SUBNETS"
+		getNetStrForDpdk $((1+$NUM_DRIVE_CONTAINERS)) $((1+$NUM_DRIVE_CONTAINERS+$NUM_COMPUTE_CONTAINERS )) "$GATEWAYS"
 		sudo weka local setup container --name compute0 --base-port 15000 --cores $NUM_COMPUTE_CONTAINERS --no-frontends --compute-dedicated-cores $NUM_COMPUTE_CONTAINERS  --memory $COMPUTE_MEMORY --failure-domain $FAILURE_DOMAIN --core-ids $compute_core_ids $net --dedicate
-		getNetStrForDpdk $(($NICS_NUM-1)) $(($NICS_NUM)) "$GATEWAYS" "$SUBNETS"
+		getNetStrForDpdk $(($NICS_NUM-1)) $(($NICS_NUM)) "$GATEWAYS"
 		sudo weka local setup container --name frontend0 --base-port 16000 --cores $NUM_FRONTEND_CONTAINERS --frontend-dedicated-cores $NUM_FRONTEND_CONTAINERS --allow-protocols true --failure-domain $FAILURE_DOMAIN --core-ids $frontend_core_ids $net --dedicate
 	else
 		sudo weka local setup container --name drives0 --base-port 14000 --cores $NUM_DRIVE_CONTAINERS --no-frontends --drives-dedicated-cores $NUM_DRIVE_CONTAINERS --failure-domain $FAILURE_DOMAIN --core-ids $drive_core_ids  --dedicate
@@ -108,7 +105,7 @@ func (d *DeployScriptGenerator) GetDeployScript() string {
 	script := fmt.Sprintf(
 		template, d.Params.VMName, d.FailureDomainCmd, d.Params.ComputeMemory, d.Params.ComputeContainerNum,
 		d.Params.FrontendContainerNum, d.Params.DriveContainerNum, d.Params.NicsNum, d.Params.InstallDpdk,
-		gateways, subnets, clusterizeFunc, protectFunc, getCoreIdsFunc, getNetStrForDpdkFunc, wekaInstallScript,
+		gateways, clusterizeFunc, protectFunc, getCoreIdsFunc, getNetStrForDpdkFunc, wekaInstallScript,
 	)
 	return dedent.Dedent(script)
 }

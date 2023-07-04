056ec3cfe5240f8bf97640290bf1a228549473b1
Assaf Giladi <assaf@weka.io>
2023-07-04 15:38:45 +0300

    feat: add report update function

---
+++ report/report.py.go
@@ -0,0 +1,27 @@
+package report
+
+import (
+	"fmt"
+	"github.com/weka/go-cloud-lib/protocol"
+	"time"
+)
+
+func UpdateReport(report protocol.Report, state *protocol.ClusterState) (err error) {
+	currentTime := time.Now().UTC().Format("15:04:05") + " UTC"
+	switch report.Type {
+	case "error":
+		if state.Errors == nil {
+			state.Errors = make(map[string][]string)
+		}
+		state.Errors[report.Hostname] = append(state.Errors[report.Hostname], fmt.Sprintf("%s: %s", currentTime, report.Message))
+	case "progress":
+		if state.Progress == nil {
+			state.Progress = make(map[string][]string)
+		}
+		state.Progress[report.Hostname] = append(state.Progress[report.Hostname], fmt.Sprintf("%s: %s", currentTime, report.Message))
+	default:
+		err = fmt.Errorf("invalid type: %s", report.Type)
+		return
+	}
+	return
+}

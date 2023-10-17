04a307cafae3bb62bf610af1b99e26715854d471
Kristina Solovyova <kristina.solovyova@weka.io>
2023-10-17 12:36:25 +0300

    feat: remove usused logic in weka_events

--- weka_events/weka_events.go
+++ weka_events/weka_events.go
@@ -3,35 +3,11 @@ package weka_events
 import (
 	"context"
 
-	"github.com/weka/go-cloud-lib/connectors"
 	"github.com/weka/go-cloud-lib/lib/jrpc"
 	"github.com/weka/go-cloud-lib/lib/types"
 	"github.com/weka/go-cloud-lib/lib/weka"
 	"github.com/weka/go-cloud-lib/logging"
 )
-
-type EmitEventParams struct {
-	Username   string
-	Password   string
-	BackendIps []string
-	Message    string
-}
-
-func EmitCustomEvent(ctx context.Context, params EmitEventParams) error {
-	jrpcBuilder := func(ip string) *jrpc.BaseClient {
-		return connectors.NewJrpcClient(ctx, ip, weka.ManagementJrpcPort, params.Username, params.Password)
-	}
-
-	jpool := &jrpc.Pool{
-		Ips:     params.BackendIps,
-		Clients: map[string]*jrpc.BaseClient{},
-		Active:  "",
-		Builder: jrpcBuilder,
-		Ctx:     ctx,
-	}
-
-	return EmitCustomEventUsingJPool(ctx, params.Message, jpool)
-}
 
 func EmitCustomEventUsingJPool(ctx context.Context, message string, jpool *jrpc.Pool) error {
 	logger := logging.LoggerFromCtx(ctx)

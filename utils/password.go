cbe8f91d3e0c58cfd80ed57df7337a34214d7757
Kristina Solovyova <kristina.solovyova@weka.io>
2024-07-17 13:21:20 +0300

    feat: remove special chars from password generation func

--- utils/password.go
+++ utils/password.go
@@ -5,11 +5,10 @@ import (
 	"time"
 )
 
-func GeneratePassword(passwordLength, minLower, minUpper, minNumbers, minSpecial int) string {
+func GeneratePassword(passwordLength, minLower, minUpper, minNumbers int) string {
 	lowerCase := "abcdefghijklmnopqrstuvwxyz" // lowercase
 	upperCase := "ABCDEFGHIJKLMNOPQRSTUVWXYZ" // uppercase
 	numbers := "0123456789"                   // numbers
-	specialChar := "!@#$%^&*()_-+={}[/?]"     // special characters
 
 	password := make([]byte, passwordLength)
 	source := rand.NewSource(time.Now().UnixNano())
@@ -31,13 +30,8 @@ func GeneratePassword(passwordLength, minLower, minUpper, minNumbers, minSpecial
 		password[minLower+minUpper+i] = numbers[randCharNum]
 	}
 
-	for i := 0; i < minSpecial; i++ {
-		randCharNum := rng.Intn(len(specialChar))
-		password[minLower+minUpper+minNumbers+i] = specialChar[randCharNum]
-	}
-
 	// Fill the remaining characters randomly
-	for i := minLower + minUpper + minNumbers + minSpecial; i < passwordLength; i++ {
+	for i := minLower + minUpper + minNumbers; i < passwordLength; i++ {
 		randNum := rng.Intn(4)
 		switch randNum {
 		case 0:
@@ -49,9 +43,6 @@ func GeneratePassword(passwordLength, minLower, minUpper, minNumbers, minSpecial
 		case 2:
 			randCharNum := rng.Intn(len(numbers))
 			password[i] = numbers[randCharNum]
-		case 3:
-			randCharNum := rng.Intn(len(specialChar))
-			password[i] = specialChar[randCharNum]
 		}
 	}
 
